c95d5d91e513d994dd15194bf4aeb7371674e7b8
olakoya <ola.koya1@gmail.com>
2024-10-22 09:56:06 +0100

    first commit

---
+++ Day1 /RealtionalOperators.py
@@ -0,0 +1,34 @@
+# a = 10 # intgers
+# b = 2
+
+#print(a>b)
+#print(a<b)
+#print(a>=b)
+#print(a<=b)
+
+
+# a = "John" # strings
+# b = "Ram"
+# print(ord('J')) # 74
+# print(ord('R')) # 82
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+
+# Comparision on strings is based on ascii code
+# print(ord('J))
+# print(ord('R))
+
+# Boolean values shouldn't be spicify in a string
+# Boolean value for True is 1
+# Boolean value for False is 0
+# print(True > True)
+# print(True >= True)
+# print(10>True)
+# print(False > True)
+# print(10>"True")
+
+# Chaining of relational operators is possible
+print(10<20<30<40>50)
+# If at least one comparison returns False then the result
