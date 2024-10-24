152f14d68c9c7a40ebccea5ab8f4b024648500bb
olakoya <ola.koya1@gmail.com>
2024-10-24 19:50:40 +0100

    updates

---
+++ Day2/Comments.py
@@ -0,0 +1,41 @@
+# Comments
+# They are descriptions about application logics
+# It's also make a code maintenance easy
+# It's a non-executable codes
+# Python will ignore comments during run time
+
+# How to specify single line comments and multi line comment
+
+# To create a functions e.g.
+# We can create a function by using the "def" keyword
+# Def function will be used to call parameters, accepting and all
+
+def my_func(): # this is single line function
+    print("Hello world") # This "def" function has some statements which is "Print hello world" ie print is specifying a statement
+my_func()
+# To call the function click on the run button to run the code
+# When the above "def" is called , it will execute the "print" statement, and the statement has some simple english letter and "hello world"
+# The above is just a sinple python program (code). It is a single line comment
+
+# To specify a single line comment:
+# Single line comment ==> ctrl or command+/ or #
+# Multi line comment ==> """" or ''''
+# Simple python program ==> is a single line comment
+
+"""
+Simple python program
+Simple python program
+Comments are ignored in these 3 double or single quotes
+
+"""
+'''
+Simple python program
+Simple python program
+Comments are ignored in these 3 double or single quotes
+'''
+"""
+Identifiers
+
+A name in pyhton is called Identifiers
+Class name or Function Name or Method Name or Variable Name or Module Name or Package Name
+"""
---
+++ Day2/Introduction.py
@@ -0,0 +1,41 @@
+# Introduction to Python - 1
+# --------------------------
+# Python is general purpose high level Programming language
+# Case sensitive Programming language
+# 	x = 10
+# 	X = 20
+# Open source Langugae 
+# Platform independent (MACOS, Windows, Linux/Unix)
+# Simpler than other programming langugaes
+
+
+# History of Python
+# -----------------
+# Python Implementation was started in December 1989 by Guido Van Rossum at Centrum Wiskunde & Informatica (CWI)  in Netherlands.
+# The inspiration for the name came from the BBC’s TV Show – ‘Monty Python’s Flying Circus’, as he was a big fan of the TV show and also he wanted a short, unique and slightly mysterious name for his invention and hence he named it Python! 
+# In February 1991, van Rossum published the code (labeled version 0.9.0).
+# In 1994, Python 1.0 was released with new features like: lambda, map, filter, and reduce.
+# Python 2.0 added new features like: list comprehensions, garbage collection system.
+# On December 3, 2008, Python 3.0 (also called "Py3K") was released. 
+
+# Python is influenced by
+# 	Functional programming features from C
+# 	Object Oriented Programming Features from C++
+# 	Scripting Language Features from Perl and Shell Script
+# 	Modular Programming Features from Modula-3
+	
+# Ways to run a Python code
+# ------------------------
+# IDLE (Python shell)
+# Command Prompt
+# notepad/notepad++
+
+
+# IDE ==> Pycharm Community Edition
+
+# .py files are called as modules
+
+# simple python program
+
+def my_func():
+    print("Hello World")
