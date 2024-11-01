78f88405e10df8fb1da362d80e125e9f2a99ecc0
olakoya <ola.koya1@gmail.com>
2024-11-01 20:08:07 +0000

    updates

---
+++ Day3/FlowControl/ControlStatement.py
@@ -0,0 +1,43 @@
+'''
+Flow Control or Control Statements in Python
+---------------------------------------------
+A flow control decides orders in which statements need to be executed
+Only few statment can be executed in all statement
+
+Types of Statements
+-------------------
+1. Conditional Statements
+2. Transfer Statements
+3. Iterative Statements or 
+4. Looping Statements
+
+Conditional Statements
+-----------------------
+When we talk about Conditional Statemnet we need to specify some conditions and that condition is Logical or Expressions conditions
+
+E.g of Logical Statements are;
+       --------------------
+        Equals: a == b
+        Not Equals: a != b
+        Less than: a < b
+        Less than or equal to: a <= b
+        Greater than: a > b
+        Greater than or equal to: a >= b
+
+There are different types of Conditional Statments and examples are;
+1. if
+2. if-else
+3. if-elif-else
+4. nested if-else
+
+Based on some conditions you perform some actions and those actions are powerful statements
+Logical conditions or expressions will return a boolean value when performed
+
+if or simple if
+----------------
+if condition (is True): (you need to perform an action)
+Action can be a statement
+We can have multiple statements e.g statment 1, statement 2, statement 3 etc. and they are simply group together 
+if condition is True statement it will be executed inside the "if or simple if" statement otherwise it won't be executed
+for every ststement in python
+'''
--- Day4/BitwiseOperators.py
+++ Day4/BitwiseOperators.py
@@ -1,6 +1,6 @@
 # Bitwise Operators
 # If we operate an operator bit by bit it's called Bitwise Operators
-# These operators are applicable for int and boolean types only. 
+# These operators are applicable for integer and boolean types only. 
 # For any other data types we will get error ie strings or float etc will give error
 
 # Operator | Name | Example | Result
@@ -41,5 +41,35 @@ Therefore,
 AND in binary of 8 4 2 1 is 0000 equals 0
 OR in binary of 8 4 2 1 is 1110 equals 14 i.e 8+4+2 = 14
 XOR in binary of 8 4 2 1 is 1110 equals 14 i.e 8+4+2 = 14
-~10 
+~10 in binary of 8 4 2 1 is 1011 equals 11 i.e 8+2+1 = 11
+<<1 in binary of 8 4 2 1 is 1011 equals 11 i.e 8+2+1 = 20
+
+~10 is nothing but a bitwise compliment
+If a number is a negative number it's always represented by 2's compliment form
+2's complemt is represented by 1's compliment+1
+1's complement
+E.g
+If we have 1 --> 0
+If w ehave 0 --> 1
+
+        MSB                            LSB
+10      0   0   0   0   1   0   1   0
+~10     1   1   1   1   0   1   0   1
+
+            1   1   1   0   1   0   1
+  1'SC      0   0   0   1   0   1   0
+        1                           1
+            0   0   0   1   0   1   1   
+<<1     0   0   0   1   0   1   0   0
+>>2     0   0   0   0   0   0   1   0     
+
+The most significant bit acts as sign bit. 0 value represents +ve number while 1 represent -ve number
++ve numbers will be represented directly in the memory where as -ve numbers will be represented indirectly in 2's complemet form
+
+P - PARENTHESIS - ()
+E - EXPONENTS - **
+D - DIVISION - /
+M - MULTIPLICATION - *
+A - ADDITION - +
+S - SUBTRACTION - -
 """
