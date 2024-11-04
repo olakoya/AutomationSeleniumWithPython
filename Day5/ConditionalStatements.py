c64463833c287c7fdb1426994dd5e47302629e1c
olakoya <ola.koya1@gmail.com>
2024-11-04 20:57:23 +0000

    updates

--- Day5/ConditionalStatements.py
+++ Day5/ConditionalStatements.py
@@ -5,18 +5,18 @@
 Before the condition there's a variable (x)
 E.g
 '''
-# x = 10 # x is a value while 10 is the variable
-# if x>5: # inside "if" body the condition can have multiple statement. We're also passing the x value inside the "x>5 memory location"
-#     print("x is greater than 5") # Output is "x is greater than 5"
-# else:
-#     print("x is less than 5")
+x = 10 # x is a value while 10 is the variable
+if x>5: # inside "if" body the condition can have multiple statement. We're also passing the x value inside the "x>5 memory location"
+    print("x is greater than 5") # Output is "x is greater than 5"
+else:
+    print("x is less than 5")
 
-# name = input("Enter Name = ")
-# if name == "Ola": # Using this "equality == operators" for comparison and to check if the statement is true
-#                  # But when use single '= operator' it is seen as an assignment of a value
-# # We use if to validate the statement as it's added above"
-#     print("Hello Ola, Good Morning")
-# print("How are you?") #storing a data inside variable
+name = input("Enter Name = ")
+if name == "Ola": # Using this "equality == operators" for comparison and to check if the statement is true
+                 # But when use single '= operator' it is seen as an assignment of a value
+# We use if to validate the statement as it's added above"
+    print("Hello Ola, Good Morning")
+print("How are you?") #storing a data inside variable
 '''
 Output is;
 Enter Name = Ola
@@ -32,23 +32,23 @@ How are you?
 
 # if-else conditional statement
 # ------------------------------
-# y = 3 # Variable "y" and Value is "3"
-# # Checking if y is an even number and to check a reminder modular division "%" operator will give a reminder
-# if y%2 == 0:
-#     print("Even Number") 
-# # if action 1 is true action 2 will be executed
-# else:
-#     print("Odd Number")
-# # Output is Odd Number
+y = 3 # Variable "y" and Value is "3"
+# Checking if y is an even number and to check a reminder modular division "%" operator will give a reminder
+if y%2 == 0:
+    print("Even Number") 
+# if action 1 is true action 2 will be executed
+else:
+    print("Odd Number")
+# Output is Odd Number
 
-# name = input("Enter Name = ")
-# if name == "Ola": # Using this "equality == operators" for comparison and to check if the statement is true
-#                  # But when use single '= operator' it is seen as an assignment of a value
-# # We use if to validate the statement as it's added above"
-#     print("Hello Ola, Good Morning")
-# else:
-#     print("Hello Guest Fine Morning")
-# print("How are you?") #storing a data inside variable
+name = input("Enter Name = ")
+if name == "Ola": # Using this "equality == operators" for comparison and to check if the statement is true
+                 # But when use single '= operator' it is seen as an assignment of a value
+# We use if to validate the statement as it's added above"
+    print("Hello Ola, Good Morning")
+else:
+    print("Hello Guest Fine Morning")
+print("How are you?") #storing a data inside variable
 '''
 Output is;
 Enter Name = Gus
@@ -56,18 +56,18 @@ Hello Guest Fine Morning
 How are you?
 '''
 
-# if-elif-else conditional statement
-# ----------------------------------
-# marks = 65 
-# # we want to check grades of student base on marks
-# if marks>=90:
-#     print("A Grade")
-# elif marks>=80:
-#     print("B Grade")
-# elif marks>=70:
-#     print("C Grade")
-# else:
-#     print("He is Failed") # default action specify in the else block
+if-elif-else conditional statement
+----------------------------------
+marks = 65 
+# we want to check grades of student base on marks
+if marks>=90:
+    print("A Grade")
+elif marks>=80:
+    print("B Grade")
+elif marks>=70:
+    print("C Grade")
+else:
+    print("He is Failed") # default action specify in the else block
 '''
 Output when used 75 for varaible is 
 C Grade
@@ -76,16 +76,16 @@ Output at 2nd execution after add the last "else" which is the default action an
 He is Failed
 '''
 
-# # How to read some favorite brand name from the user by using the input method?
-# brand = input("Enter Your Favourite Brand: ")
-# if brand == "RC":
-#     print("This is Children's brand")
-# elif brand == "KF":
-#     print("It is not that much kick")
-# elif brand == "FO":
-#     print("Buy one get one free")
-# else:
-#     print("Other brands are not recommended")
+# How to read some favorite brand name from the user by using the input method?
+brand = input("Enter Your Favourite Brand: ")
+if brand == "RC":
+    print("This is Children's brand")
+elif brand == "KF":
+    print("It is not that much kick")
+elif brand == "FO":
+    print("Buy one get one free")
+else:
+    print("Other brands are not recommended")
 '''
 Output 1 is
 Enter Your Favourite Brand: MK
