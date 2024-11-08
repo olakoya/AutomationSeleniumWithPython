83e37aab1530cf08bcd1ed78fde67627e1b8673a
olakoya <ola.koya1@gmail.com>
2024-11-08 18:43:58 +0000

    updates

--- Day2/DataTypes_Notes.py
+++ Day2/DataTypes_Notes.py
@@ -1,19 +1,22 @@
 '''
 Data Types in Python:
-There are lots of datat types in python.
+----------------------
+There are lots of data types in python.
 Data Type represents the type of data present inside a variable.
 In python we are not required to specify the type explicity . 
 Based on value provided, the type will be assigned automatically, hence python is dynamically Typed language.
 
 Python contains several inbuilt functions:
+-------------------------------------------
 1. type() => to check the type of variable
 2. id() => to get address of object
 3. print() => to print the value
-In python everything is an object including dot (.), (,) etc. anyhting is treated as an object in python
+In python everything is an object including dot (.), (,) etc. anything is treated as an object in python
 
 Python Data Types are:
+-----------------------
 Numbers: Int | Float | Complex
-Bool: True | False
+Boolean: True | False
 Sequence: String | List | Tuple
 Mapping: Dict
 Sets: Set | FrozenSet
