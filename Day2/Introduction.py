724f7049a94bb6271adad22c5726c84d5739a660
olakoya <ola.koya1@gmail.com>
2024-11-08 19:07:14 +0000

    updates

--- Day2/Introduction.py
+++ Day2/Introduction.py
@@ -1,42 +1,57 @@
-print("Welcome back Ola .. This is refreshing my Python knowledge")
-# Introduction to Python - 1
-# --------------------------
-# Python is general purpose high level Programming language
-# Case sensitive Programming language
-# 	x = 10
-# 	X = 20
-# Open source Langugae 
-# Platform independent (MACOS, Windows, Linux/Unix)
-# Simpler than other programming langugaes
 
+print("Welcome back Ola .. This is to refresh my Python basic knowledge")
 
-# History of Python
-# -----------------
-# Python Implementation was started in December 1989 by Guido Van Rossum at Centrum Wiskunde & Informatica (CWI)  in Netherlands.
-# The inspiration for the name came from the BBC’s TV Show – ‘Monty Python’s Flying Circus’, as he was a big fan of the TV show and also he wanted a short, unique and slightly mysterious name for his invention and hence he named it Python! 
-# In February 1991, van Rossum published the code (labeled version 0.9.0).
-# In 1994, Python 1.0 was released with new features like: lambda, map, filter, and reduce.
-# Python 2.0 added new features like: list comprehensions, garbage collection system.
-# On December 3, 2008, Python 3.0 (also called "Py3K") was released. 
+"""
+Introduction to Python - 1
+--------------------------
+Python is general purpose high level Programming language.
+It is a Case sensitive Programming language.
+	x = 10
+	X = 20
+It is an Open source Language.
+It is Platform independent (MACOS, Windows, Linux/Unix).
+It is Simplier than any other programming langugaes.
 
-# Python is influenced by
-# 	Functional programming features from C
-# 	Object Oriented Programming Features from C++
-# 	Scripting Language Features from Perl and Shell Script
-# 	Modular Programming Features from Modula-3
+History of Python
+-----------------
+Python Implementation started in December 1989 by Guido Van Rossum at Centrum Wiskunde & Informatica (CWI) in Netherlands.
+The inspiration for the name came from the BBC’s TV Show – ‘Monty Python’s Flying Circus’, as he was a big fan of the TV show 
+and also he wanted a short, unique and slightly mysterious name for his invention and hence he named it Python! 
+In February 1991, van Rossum published the code (labeled version 0.9.0).
+In 1994, Python 1.0 was released with new features like: lambda, map, filter, and reduce.
+Python 2.0 added new features like: list comprehensions, garbage collection system.
+On December 3, 2008, Python 3.0 (also called "Py3K") was released. 
+
+Python is influenced by
+------------------------
+Functional programming features is from C
+Object Oriented Programming Features is from C++
+Scripting Language Features is from Perl and Shell Script
+Modular Programming Features is from Modula-3
 	
-# Ways to run a Python code
-# ------------------------
-# IDLE (Python shell)
-# Command Prompt
-# notepad/notepad++
+Ways to run a Python code
+------------------------
+IDLE (Python shell)
+Command Prompt
+notepad/notepad++
 
+Python IDE ==> Pycharm Community Edition
 
-# IDE ==> Pycharm Community Edition
+.py files are called as modules
 
-# .py files are called as modules
-
-# simple python program
-
+simple python program
+"""
 def my_func():
     print("Hello World")
+my_func()
+
+def my_name():
+    print("Ola Koya")
+my_name()
+
+'''
+Output for the above codes are 
+Welcome back Ola .. This is to refresh my Python basic knowledge
+Hello World
+Ola Koya
+'''
