c32cddfb94c786bab2888c457ba4aeebfbd0e26f
olakoya <ola.koya1@gmail.com>
2024-11-08 18:51:12 +0000

    updates

--- Day2/Identifiers.py
+++ Day2/Identifiers.py
@@ -3,15 +3,15 @@
 
 """
 Identifiers
-
+-------------
 A name in pyhton is called Identifiers
-Class name or Function Name or Method Name or Variable Name or Module Name or Package Name
+Class name, Function Name, Method Name, Variable Name, Module Name or Package Name are all identifiers
 
 Rules for Creating Identifiers
-
-Their only allowed characters are
-    alphabet symbols (lower or upper case)
-    digits (0 to 9) digits can't be used as a beginer
+-------------------------------
+The only allowed characters for identifiers are
+    alphabet symbols (lower (a,b,c) or upper case(A,B,C))
+    digits (0 to 9) digits can't be used as a beginner
     underscore score(_)
 Dollar($) sysmbol is not allowed in python and it's a special symbol
 Identifier should not start wih digits
@@ -26,9 +26,8 @@ name ==> starts with _symbol ==> private (Attribute that starts with underscore 
 name ==> starts with _ _ symbol ==> strongly private
 Magic method starts with double underscores symbols e.g _ _ add _ _ ==> magic methods (can't be override)
 Identifier can't be created randomly. There should be some meaning to a name. 
-To create a name one needs to follow some rules. Only an old member characters that are alphabet symbols begins with an underscore
+To create a name one needs to follow some rules. 
+Only an old member characters that are alphabet symbols begin with an underscore
 Keywords are sensitive functionality
-
-
 
 """
