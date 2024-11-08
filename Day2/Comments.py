69e89ae2dfb84de52eb913fac5a80d6a83410e96
olakoya <ola.koya1@gmail.com>
2024-11-08 18:38:31 +0000

    updates

--- Day2/Comments.py
+++ Day2/Comments.py
@@ -6,16 +6,21 @@
 
 # How to specify single line comments and multi line comment
 
-# To create a functions e.g.
-# We can create a function by using the "def" keyword
+# To create a functions
+#-----------------------
+# E.g We can create a function by using the "def" keyword
 # Def function will be used to call parameters, accepting and all
 
 def my_func(): # this is single line function
     print("Hello world") # This "def" function has some statements which is "Print hello world" ie print is specifying a statement
 my_func()
+'''
+Output is 
+Hello world
+'''
 # To call the function click on the run button to run the code
 # When the above "def" is called , it will execute the "print" statement, and the statement has some simple english letter and "hello world"
-# The above is just a sinple python program (code). It is a single line comment
+# The above is just a simple python program (code). It is a single line comment
 
 # To specify a single line comment:
 # Single line comment ==> ctrl or command+/ or #
@@ -32,4 +37,11 @@ Comments are ignored in these 3 double or single quotes
 Simple python program
 Simple python program
 Comments are ignored in these 3 double or single quotes
+'''
+def my_name():
+    print("My name is Ola Koya")
+my_name()
+'''
+Output is 
+My name is Ola Koya
 '''
