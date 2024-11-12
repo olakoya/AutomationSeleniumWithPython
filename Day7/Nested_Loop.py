7543b8cd287e50d69f097cbc9c3601715bde3311
olakoya <ola.koya1@gmail.com>
2024-11-12 18:59:32 +0000

    updates

--- Day7/Nested_Loop.py
+++ Day7/Nested_Loop.py
@@ -14,16 +14,3 @@ Output is
 4 * 2 = 8       4 * 3 = 12 4 * 4 = 16       4 * 5 = 20  
 '''
 
-'''
-Transfer statements or Jumping Statements
------------------------------------------
-Types of Statements are
-1. Break
-    We can use break statement inside loops to break loop execution based on some condition.
-2. Continue
-    We can use continue statement inside loops to skip loop execution based on some condition.
-3. Pass
-    Pass is a keyword in Python.
-    In our programming syntactically if block is required which won't do anything then we can 
-    define the empty block with pass keyword
-'''
---
+++ Day7/Transfer_Statements.py
@@ -0,0 +1,13 @@
+'''
+Transfer statements or Jumping Statements
+-----------------------------------------
+Types of Statements are
+1. Break
+    We can use break statement inside loops to break loop execution based on some condition.
+2. Continue
+    We can use continue statement inside loops to skip loop execution based on some condition.
+3. Pass
+    Pass is a keyword in Python.
+    In our programming syntactically if block is required which won't do anything then we can 
+    define the empty block with pass keyword
+'''
