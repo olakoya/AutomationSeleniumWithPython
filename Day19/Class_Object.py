527c95ad3f4063064e1af9d3f16354161284aee6
olakoya <ola.koya1@gmail.com>
2024-11-22 14:30:35 +0000

    updates

---
+++ Day19/Class_Object.py
@@ -0,0 +1,48 @@
+'''
+Class and Object
+----------------
+
+Types of Object Oriented Programming Languages Principles (OOPS)
+-----------------------------------------------------------------
+1. Class and object
+2. Inheritance
+3. Abstraction
+4. Polymorphism
+5. Encapsulations
+
+
+Types/Structures of Programming Langugages
+-------------------------------------------
+1. Structured: This programe doesn't support object oriented programming principles => C, Python
+2. Object Based: This supports only class and object => VB, VBScript, Python
+3. Object Oriented: This supports OOPS ==> Java, C++, C##, Python
+
+In Python, without creating a class one can write a code.
+ALso with class and object one can write a code in Python.
+Python is fully object oriented programming language.
+
+'''
+'''
+Class
+------
+Class is nothing but a blueprint
+A blueprint is a design called a class
+In Python everything is treated as an object therefore, we need to create an objec
+To create an object one needs a blueprint
+Blueprint is a logical entity that is, it will not occupy space in the memory 
+Except for physical entities and that will only occupy some space in the memory
+If you want to build a house we need a design
+The design document will occupy some space
+And this design document is nothing but a class which is a blueprint logical memory
+That is it wouldn't occup any space in the memory
+You create an object and that object is your house
+The house consists of bedroom, kitchen, bathroom, toilet etc and as such one will require some requirements/properties
+A class contains properties and operations
+With the help of a blueprint one will create an object
+From Object there will be a specification
+And those specifications are called properties and operations of an object
+These properties are also called attributes while operations can be called actions
+With the help of class one creates an object and for the object one will have some specific properties
+
+
+'''
