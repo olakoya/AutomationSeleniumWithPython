b0f19cff8a4eb5a93b986ef6ac5cd356a1f6d4d0
olakoya <ola.koya1@gmail.com>
2024-11-23 02:43:55 +0000

    updates

--- Day19/Class_Object_Notes.py
+++ Day19/Class_Object_Notes.py
@@ -25,7 +25,7 @@ Python is fully object oriented programming language.
 '''
 Class
 ------
-Class is nothing but a blueprint
+Class is nothing but a design of blueprint
 A blueprint is a design called a class
 In Python everything is treated as an object therefore, we need to create an objec
 To create an object one needs a blueprint
@@ -43,6 +43,47 @@ From Object there will be a specification
 And those specifications are called properties and operations of an object
 These properties are also called attributes while operations can be called actions
 With the help of class one creates an object and for the object one will have some specific properties
+Class consists of variables and methods. In blueprint we specify variables and objects
+Properties or Attributes of objects are called Variables
+'''
+'''
+Object
+-------
+Object is a physical existence of a class
+Object will occupy any space in the memory
+We create a class with a def keyword
 
+Syntax
+------
+    i. We create a keyword ==> class
+    ia. Class returns consists of "Documentation string" and just a comment which is optional
+    ii. Followed by class name ==> class_name
+    iii. Inside class we have varaibles ==> variables
+    iv. And methods of an object ==> methods
+How to call a function? We need a function name with an open and close parenthesis ==> class_name()
+We call a class with the help of class_name and  open and close parentheses ()
+If we want to specify the object we call the class name
+We need a physical existence of a class to create an object.
+Function is an entry point and call it with the help of keyword
+And calling the class is creating an object
 
+        class class_name: # class creation
+                "documentation string"
+                variables
+                methods
+        
+        class name() # class calling => object of a particular class
+'''
+'''
+Method
+-------
+Method is a function created inside a class
+
+self is a parameter which is referring to class where the method is defined
+
+Once we create a class we can create any number of objects and this objects are independent
+
+To objects we can give reference and this is independent 
+
+With the help of refrence variable we call variables and methods of object
 '''
---
+++ Day19/class_object_1.py
@@ -0,0 +1,59 @@
+'''
+class class_name: # class creation
+                "documentation string"
+                variables
+                methods
+        
+        class name() # class calling => object of a particular class
+'''
+# creation of a class
+
+class Test: # class creation
+    a = 10 # class variables
+    def test(self): # function created in a class is called mathod i.e creating a method
+        print(self.a) # this is accessing the class variable # self is helping to access
+
+t1 = Test() # object
+t2 = Test() # object
+t3 = Test() # object
+t4 = Test() # object
+t5 = Test() # object
+t6 = Test() # object
+t7 = Test() # object
+
+print(t1.a) # to see a data present in "a = 10", print will help in the access of variable "a" with the object "t1"
+t1.test() # we are access an object "t1.test()" with the help of "print(t1.a)" and print out the value in a = 10
+print(t2.a)
+t2.test() 
+
+'''
+Output is
+10
+10
+10
+10
+'''
+
+'''
+Method is a function created inside a class and class is a blueprint
+
+self is a parameter which is referring to class where the method is defined
+
+Inside "class Test:" this "def test(self):" is defined i.e. 
+To say "def test(self):" is a property of this "class Test:" we need to use the "self" parameter
+And "self" is a mandatory parameters
+Once it's created we to specify this "def test(self):" one would get an error
+And when a class variable "a = 10" is created one can access it through "print(self.a)"
+To make our class physically appeared we need to create an object "Test" with open close parenthensis
+For instance in building a house we need to design it before construction
+Therefore designing of the house is class method while construction is the test() object
+For single class we can create any total number of object
+Once we create a class we can create any number of objects and this objects are independent
+Which means with a single design we can construct many number of houses from that one design 
+And all these houses are stand alone and independent despite having the same design
+Then through these objects (Test()) they can have different reference variables (t1 = ) i.e. all these houses will have different addresses
+With the help of the reference (t1 = ), one can access the variable (a = 10) and method of the class
+To see a data present in "a = 10", print will help in the access of variable "a" with the object "t1"
+We are access an object "t1.test()" with the help of "print(t1.a)" and print out the value in a = 10
+With the help of "t1.test()" we are calling the method "print(self.a)"
+'''
