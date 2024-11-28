5189d99a639388293a9adb9192cec98d4f841675
olakoya <ola.koya1@gmail.com>
2024-11-28 22:52:54 +0000

    updates

--- Day19/Class_Object_Notes.py
+++ Day19/Class_Object_Notes.py
@@ -45,6 +45,19 @@ These properties are also called attributes while operations can be called actio
 With the help of class, one can create an object and for the object one will have some specific properties
 Class consists of variables (properties) and methods (behaviour). In blueprint we specify variables and objects
 Properties or Attributes of objects are called Variables
+E.g
+        Class Test ==> A class which is a creation/declaration of a blueprint
+                a = 10 ==> inside class we have some class variables a = 10
+                def m1(self): ==> for a variable we have a method with parenthese open and close def m1(self):
+                    print(self.a) ==> inside the method we are accessing a particular variable which we've defined
+Test() ==> Is an object or an instance of a class
+t ==> on=bject reference variable
+class ==> blueprint
+object ==> house
+object reference variable ==> house address
+
+Class consists of a variable and a method
+
 '''
 '''
 Object
@@ -79,29 +92,47 @@ Method
 -------
 Method is a function created/declared inside a class
 
+That is, Method is a property of class
+
 We create function by using a def keyword, function name, open and close parentheses and colon 
 
 By default method has a self parameter which is nothing but a function deeclared inside of method
 
 If self is not declared it becomes an issue because it's property of a particular class
 
-self is a parameter which is referring to class where the method is defined
+Self is a mandatory parameter for a method of a class that is, a class where the method is defined
 
 Once we create a class we can create any number of objects and this objects are independent
 
 To objects we can give reference and this is independent 
 
-With the help of refrence variable we call variables and methods of object
+With the help of reference variable, we call variables and methods of object
 '''
 
 '''
 Types of Methods
 -----------------
 1. Instance Method
-        This is use to update or retrieve instance-specific data, like modifying a user's profile details
+        This is use to update or retrieve instance-specific data, like modifying a user's profile details.
 2. Class Method
-        This is use for for class-wide operations, like keeping track of the total number if the users in a system
+        This is use for for class-wide operations, like keeping track of the total number if the users in a system.
 3. Static Method
-        This is use for utitlity functions, like validating a username format with out needing class or instance data
+        This is use for utitlity functions, like validating a username format with out needing class or instance data.
+        Utilities consists of reusable functions
 
+'''
+'''
+Constructor
+------------
+Constructor is a special type of method with unique name __init__(self is optional)
+To declare and initialize instance variables
+Once we create an object constructor is automatically invoked or executed
+E.g
+        Class Test ==> A class which is a creation/declaration of a blueprint
+                a = 10 ==> inside class we have some class variables a = 10
+                def __init__(self): ==> creating a constructor with a self parameter
+                    print(self.a) ==> inside the method we are accessing a particular variable which we've defined
+                def m1(self): ==> for a variable we have a method with parenthese open and close def m1(self):
+                    print(self.a) ==> inside the method we are accessing a particular variable which we've defined
+Test() ==> Is an object or an instance of a class and if constructor is in the method print or output will be 10
 '''
