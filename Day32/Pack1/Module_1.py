caa0fe64e2ceef35dd16ea09805845b11ead7886
olakoya <ola.koya1@gmail.com>
2024-12-12 18:15:04 +0000

    "updates"

---
+++ Day32/Pack1/Module_1.py
@@ -0,0 +1,9 @@
+def display():
+    print("This is display function from module1")
+
+display()
+
+'''
+Output is 
+This is display function from module1
+'''
---
+++ Day32/Pack1/Module_2.py
@@ -0,0 +1,9 @@
+def show():
+    print("This is show function from module2")
+
+show()
+
+'''
+Output is 
+This is show function from module2
+'''
