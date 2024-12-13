9c897ac71b529a17ce9daf5496f67b1fa91e1f02
olakoya <ola.koya1@gmail.com>
2024-12-13 16:23:38 +0000

    "updates"

---
+++ Day33/Exception.py
@@ -0,0 +1,23 @@
+'''
+Exception handling in Python
+-----------------------------
+In python with the help of the exception handling concepts one can easily handles exceptions
+
+Types of Errors
+---------------
+The most common errors in Python are;
+1. Syntax Errors
+2. Runtime Errors
+
+1. Syntax Errors
+------------------
+These are errors that occur due to invalid syntax
+
+2. Runtime Errors
+------------------
+When writing logic and due to memory issues this error is called runtime error
+That is errors due to wrong end user input, memory problems and invalid programming logic
+
+Developers are responsible for the correction of syntax errors
+
+'''
