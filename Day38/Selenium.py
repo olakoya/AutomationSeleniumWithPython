1f9bb5f44eb9bc0ff5d836467ec7080f82557d78
olakoya <ola.koya1@gmail.com>
2024-12-24 14:10:53 +0000

    "updates"

---
+++ Day38/Selenium.py
@@ -0,0 +1,45 @@
+'''
+Introduction to Selenium WebDriver
+-----------------------------------
+Selenium contains a Suite and this suite contains component
+Selenium is a single tool
+
+Selenium Suite Components
+---------------------------
+    i. Selenium IDE ==> This is a browser extension and can be called record anf playback tool
+    ii. Selenium WebDriver ==> This is the most important tool and has a combination of tools. It's used for automation purpose and
+     interact with browser with the help of programming language. And this is most important, popular and powerful tool
+    iii. Selenium RC ==> It's combined with selenium IDE itself
+    iv. Selenium Grid ==> This is used for parallel testing on multiple platforms and browser
+
+Selenium is a package
+WebDriver is a module
+Module contains Functions and Classes
+
+Package contains => Modules
+Modules contains => Functions and Classes
+
+Chrome Browser calls ==> Chrome class()
+Edge Browser calls ==> Edge class()
+Firefox Browser ==> Firefox class()
+
+Selenium WebDriver is only used to automate GUI or web application
+Selenium WebDriver cannot automate APIs and DeskTop Application
+WebDriver is an programming interface and it's act like an API
+Every web application in this modean world follows client - server architecture
+
+3 Types of Layers
+-----------------=
+Presentation Layer
+API Layer
+Backend Layer
+
+Setting Up and Installation
+----------------------------
+1. Python ==> A programming language
+2. Pycharm ==> Clients Library
+Selenium WebDriver ==> Automation tools
+
+Opening on Pycharm for new project
+
+'''
