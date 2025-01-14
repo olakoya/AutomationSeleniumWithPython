f51425c5e04d680784d7e18411efbf87ac57c456
olakoya <ola.koya1@gmail.com>
2025-01-14 21:30:45 +0000

    updates

---
+++ Day12/String-4.py
@@ -0,0 +1,197 @@
+# Counting Substring
+s = "Donald Trump Donald"
+sub = 'Donald'
+
+print(s.count(sub)) # function without beginning and ending
+'''
+Output is 
+2
+'''
+
+'''
+Replacing a string with another string
+---------------------------------------
+Replace (oldsrting, newstring i.e passing as a parameter)
+
+E.g
+'''
+s = "Learning python is very difficult"
+s1 = s.replace("difficult", "easy") # string is an immutable data type
+print(s)
+print(s1)
+'''
+Output is
+Learning python is very easy
+
+Output after adding line 20
+Learning python is very difficult
+Learning python is very easy
+'''
+'''
+Once we creates string object, we cannot change the content.
+This is non changeable behaviour is nothing but immutability.
+If we are trying yo change the content by using any method, then with those changes a new object will be created and changes 
+won't happened in existing object.
+Hence with replace() method also, a new object that will be created but existing object won't be changed 
+Example is on the line of codes above in 18 to 21
+
+Mutable means can change
+Immutable means unchanged e.g string
+
+----------------------------------------------------
+'''
+
+'''
+Split the strings
+----------------
+s.split(separator)
+
+s = "05/November/2024" this is a string immutable
+To split the date above, what to do is add the below code line
+s.split("/")
+s.split("-")
+
+By default separator is a space and vice versa 
+After splitting it will return List of Data Structure
+E.g
+'''
+
+# Splitting the string
+s = "Learning python is very very interesting"
+print(s.split())
+'''
+Output is 
+['Learning', 'python', 'is', 'very', 'very', 'interesting']
+'''
+# E.g 2
+s = "22-10-2024"
+print(s.split("-"))
+'''
+Output is
+['22', '10', '2024']
+'''
+# E.g 3
+s = "22-$10-2024"
+print(s.split("-"))
+'''
+Output is
+['22', '$10', '2024']
+
+--------------------------------
+'''
+
+'''
+Join the Strings
+------------------
+join(sequence)
+
+Sequence can be a ==> range, string, list, tuple, set, dictionary data structure 
+
+join(strings) based on some separator it can be joined
+Strings means group of strings
+separator.join(group of strings)
+
+E.g
+'''
+# Join the Strings
+print(list(range(10)))
+'''
+Output is
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+'''
+
+print(str(list(range(10))))
+'''
+Output is
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+'''
+
+print(type(str(list(range(10)))))
+'''
+Output is
+<class 'str'>
+'''
+
+print(("-".join(s)))
+'''
+Output is
+2-2---$-1-0---2-0-2-4
+'''
+
+s = {"Donald Trump", "Kamala Harris", "Vivek Ramaswany", "Barack Obama"} # calibraces
+print(("-".join(s)))
+'''
+Output is
+Vivek Ramaswany-Donald Trump-Kamala Harris-Barack Obama
+'''
+
+s = ["Donald Trump", "Kamala Harris", "Vivek Ramaswany", "Barack Obama"] # square brackets
+print(("-".join(s)))
+'''
+Output is
+Donald Trump-Kamala Harris-Vivek Ramaswany-Barack Obama
+'''
+
+s = ["Donald Trump", "Kamala Harris", "Vivek Ramaswany", "Barack Obama"] # square brackets
+print(("/".join(s))) # using slash
+'''
+Output is
+Donald Trump/Kamala Harris/Vivek Ramaswany/Barack Obama
+
+------------------------------------------------------------
+'''
+
+'''
+Change the case of the strings
+-------------------------------
+Upper() ==> To convert all characters to upper case
+Lower() ==> To convert all characters to lower case
+Swapcase() ==> To convert all lower case characters to upper case and vice versa
+Title() ==> To convert all characters to title case i.e. first character in every word should be upper case and all 
+            remaining characters should be in lower case
+Capitalise() ==> Only first character will be converted to upper case and all remaining characters can be
+                converted to lower case
+E.g
+'''
+# Changing case of the strings
+s = "Learning python is very very interesting"
+print(s.upper())
+print(s.lower())
+print(s.swapcase())
+print(s.title())
+print(s.capitalize())
+'''
+Output is
+LEARNING PYTHON IS VERY VERY INTERESTING
+learning python is very very interesting
+lEARNING PYTHON IS VERY VERY INTERESTING
+Learning Python Is Very Very Interesting
+Learning python is very very interesting
+
+---------------------------------------------------
+'''
+
+'''
+Checking presence of Substring at the beginning and ending of a string
+------------------------------------------------------------------------
+startswith(substring)
+endswith(substring)
+E.g
+'''
+s = "abcdefgh"
+sub = "a"
+
+print(s.startswith(sub))
+print(s.endswith(sub))
+'''
+Output is
+True
+False
+
+----------------------------------
+'''
+
+'''
+Checking type of characters present in the string
+----------------------------------------------------
+'''
